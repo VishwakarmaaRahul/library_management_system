dc44da009c670b3c7c8e059f319f48b5a418c1e3
Rahul Sharma <rahulsharmados@gmail.com>
2025-08-03 20:22:50 +0530

    add api for books

--- phase2_data_ingestion/fetching-data-from-api/fetching_library_data.py
+++ phase2_data_ingestion/fetching-data-from-api/fetching_library_data.py
@@ -43,26 +43,50 @@ from bs4 import BeautifulSoup
 
 #
 # url = "https://openlibrary.org/search.json?q=Gitanjali"
+# url = "https://openlibrary.org/search/authors.json?q=Rabindranath Tagore"
+# r = requests.get(url)
+# data = r.json()
+# # print(data)
+#
+# for index, author in enumerate(data.get("docs", [])[:3], 1):
+#     print(index, author)
+#     # Loop through each field in the author object
+#     for key, value in author.items():
+#         print(f"{key}: {value}")
+#         url = f"https://openlibrary.org/works/value.json"
+#         response = requests.get(url)
+#         author_data = response.json()
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         print(soup.prettify())
+
+
+import requests
+from bs4 import BeautifulSoup
+
 url = "https://openlibrary.org/search/authors.json?q=Rabindranath Tagore"
 r = requests.get(url)
 data = r.json()
-# print(data)
 
-for index, author in enumerate(data.get("docs", []), 1):
-    print(f"\nAuthor {index}:")
+for index, author in enumerate(data.get("docs", [])[:3], 1):
+    print(f"\nAuthor #{index}")
 
-    # Loop through each field in the author object
     for key, value in author.items():
         print(f"{key}: {value}")
 
+    # Get author key and fetch author details
+    author_key = author.get("key")  # e.g., "OL1770A"
 
+    if author_key:
+        detail_url = f"https://openlibrary.org/authors/{author_key}.json"
+        response = requests.get(detail_url)
 
-#
-# url = f"https://openlibrary.org/works/OL1770A.json"
-# response = requests.get(url)
-# author_data = response.json()
-# soup = BeautifulSoup(response.text, 'html.parser')
-# # print(soup.prettify())
+        if response.status_code == 200:
+            author_data = response.json()
+            print("\nAuthor JSON data from detailed endpoint:")
+            print(author_data)
+        else:
+            print(f"Failed to fetch details for {author_key}")
+
 #
 # print(author_data["key"])
 # print(author_data["name"])
@@ -133,5 +157,23 @@ for index, author in enumerate(data.get("docs", []), 1):
 
 
 
+import requests
+import json
 
+url = "https://openlibrary.org/books/OL27187262M.json"
+headers = {
+    "User-Agent": "MyApp/1.0 (rahulvishwakarmnados@gmail.com)"
+}
+response = requests.get(url, headers=headers)
+data = response.json()
+
+# print(data)
+print(f"Book Key: {data.get('key')}")
+print(f"Title: {data.get('title')}")
+print(f"Author: {data.get('authors')}")
+print(f"Publish Date: {data.get('publish_date', 'Unknown')}")
+print(f"ISBN_10: {data.get('isbn_10')}")
+print(f"ISBN_13: {data.get('isbn_13')}")
+print(f"Description: {data.get('description', {}).get('value', 'No description available')}")
+# print(json.dumps(data, indent=2))
 
