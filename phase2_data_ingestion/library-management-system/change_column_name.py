91deddb5095450cfa899a6e3669baef7efc0361c
VishwakarmaaRahul <165124078+VishwakarmaaRahul@users.noreply.github.com>
2025-08-04 17:28:38 +0530

    Delete phase2_data_ingestion/library-management-system/change_column_name.py

--- phase2_data_ingestion/library-management-system/change_column_name.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("csv_data/BooksOLD.csv")
-
-# Rename columns using a dictionary: {"old_name": "new_name"}
-df.rename(columns={"publication_date": "published_date"}, inplace=True)
-
-# Save the updated CSV
-df.to_csv("csv_data/BooksOLD.csv", index=False)
