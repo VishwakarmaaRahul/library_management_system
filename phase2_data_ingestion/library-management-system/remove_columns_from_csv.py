c8b3ba355f3a82309ec341830aa70f8a1d38257f
VishwakarmaaRahul <165124078+VishwakarmaaRahul@users.noreply.github.com>
2025-08-04 17:29:27 +0530

    Delete phase2_data_ingestion/library-management-system/remove_columns_from_csv.py

--- phase2_data_ingestion/library-management-system/remove_columns_from_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("csv_data/BooksOLD.csv")
-
-df.drop(columns=["book_id", "total_copies","available_copies","library_id"], inplace=True)
-
-df.to_csv("Books.csv", index=False)
