1ab25f14d6ac16b67258ac633d97d10ac52e018a
RahulVishwakarma <rahulsharmados@gmail.com>
2025-08-21 17:57:15 +0530

    add testing

---
+++ phase3_django_api/librari_api/library_management_system/settings/testing.py
@@ -0,0 +1,20 @@
+# library_management_system/settings/testing.py
+from .base import *
+
+DEBUG = True
+SECRET_KEY = "test-secret-key"
+
+# Use in-memory database for tests
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": ":memory:",
+#     }
+# }
+
+# Faster password hashing during tests
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+ALLOWED_HOSTS = ["*"]
